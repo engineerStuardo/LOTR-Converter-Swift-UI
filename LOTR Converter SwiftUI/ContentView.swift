b4de493030179710ea66c5efb70347f933e1f769
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-11-21 10:28:24 -0600

    Initial Commit

---
+++ LOTR Converter SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTR Converter SwiftUI
+//
+//  Created by Italo Stuardo on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR Converter SwiftUI/LOTR_Converter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_Converter_SwiftUIApp.swift
+//  LOTR Converter SwiftUI
+//
+//  Created by Italo Stuardo on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_Converter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
